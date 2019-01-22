769c5b1bacf3d43eb3f89626fdec3c1306b5125f
Zavrajnaia Marina <marina.zavrajnaia@outlook.com>
2019-01-22 19:59:55 +0200

    Задание №3 - Рефакторинг, создан вспомогательный класс для хранения свойств группы контактов, этот класс используется для передачи данных в во вспомогательные методы

---
+++ addressbook-web-tests/addressbook-web-tests/GroupData.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    class GroupData
+    {
+        private string name; // field
+        private string header = "";
+        private string footer = "";
+
+        public GroupData(string name) // constructor
+        {
+            this.name = name;
+        }
+
+        public string Name // property
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string Header
+        {
+            get
+            {
+                return header;
+            }
+
+            set
+            {
+                this.header = value;
+            }
+        }
+
+        public string Footer
+            {
+                get
+                  {
+                return footer;
+                  }
+
+                set
+                {
+                footer = value;
+                }
+            }
+    }
+}
